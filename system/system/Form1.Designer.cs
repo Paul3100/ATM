11915fa9e4601d50b40e271aa9092c7f22510d08
Paolo Agyei <agyei31@live.com>
2024-08-08 09:14:53 +0100

    atm initialisation

---
+++ system/system/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace system
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pin = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pin
+            // 
+            this.pin.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pin.Location = new System.Drawing.Point(178, 149);
+            this.pin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pin.Multiline = true;
+            this.pin.Name = "pin";
+            this.pin.Size = new System.Drawing.Size(249, 41);
+            this.pin.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(394, 266);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 60);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(175, 125);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "PIN";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pin);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox pin;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ system/system/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace system
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        SqlCommand command;
+      
+        String sql = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            initialise_table();
+        }
+        private void initialise_table()
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            // Specify Data source
+            string cs = @"Data Source=tcp:manipulate.database.windows.net,1433;Initial Catalog=atm;Persist Security Info=False;User ID=atm;Password=Awork1hard;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;";
+            conn = new SqlConnection(cs);
+            conn.Open();
+            Console.WriteLine("Connected");
+            // Delete prior records
+            sql = "DELETE FROM dbo.atmsys;";
+            command = new SqlCommand(sql, conn);
+            adapter.InsertCommand = new SqlCommand(sql, conn);
+            adapter.InsertCommand.ExecuteNonQuery();
+            command.Dispose();
+            // Initialise new pin numbers
+            for (int i = 1000; i < 10000; i+=1000)
+            {
+                sql = "Insert into atmsys (pin,cash,transactions) values ("+i+",500, '[]' )";
+                command = new SqlCommand(sql, conn);
+                adapter.InsertCommand = new SqlCommand(sql, conn);
+                adapter.InsertCommand.ExecuteNonQuery();
+
+                command.Dispose();
+            }
+
+            Console.WriteLine("Records Inserted");
+        }
+        private bool pin_check(int pin)
+        {
+            SqlDataReader dataReader;
+            sql = "SELECT * FROM dbo.atmsys WHERE pin="+pin+";";
+            command = new SqlCommand(sql, conn);
+            dataReader = command.ExecuteReader();
+
+            // Valid Pin
+            if (dataReader.HasRows)
+            {
+                dataReader.Close();
+                return true;
+            }
+
+            else // Invalid
+            {
+                dataReader.Close();
+                return false;
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int pin_num = Int32.Parse(pin.Text);
+                if (pin_check(pin_num))
+                {
+                    Form2 form = new Form2();
+                    form.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Pin");
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Invalid Pin");
+            }
+            
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+/*
+ * atm
+ * Awork1hard
+ * manipulate.database.windows.net
+ */
